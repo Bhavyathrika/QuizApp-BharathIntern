a17808f93cd7b782979f61d1fa871ea873ffa03f
Bhavyathrika <bhavyathrikasankuri@gmail.com>
2023-11-09 21:48:26 +0530

    init commit

---
+++ app/src/main/java/com/example/qiuz/queandans.java
@@ -0,0 +1,35 @@
+package com.example.qiuz;
+
+public class queandans {
+    public static String ques[]= {
+            "Which of the following type of class allows only one object of it to be created?",
+            "Which of the following is not a type of constructor?",
+            "Which of the following is not the member of class?",
+            "Which of the following concepts means determining at runtime what method to invoke?",
+            "Which of the following term is used for a function defined inside a class?",
+            "How many instances of an abstract class can be created?",
+            "Which of the following cannot be friend?"
+    };
+
+
+    public static String opn[][]={
+            {"Virtual class", "Abstract class", "Singleton class", "Friend class"},
+            {"Copy constructor","Friend constructor" ,"Default constructor" ,"Parameterized constructor"},
+            {"Static function","Friend function","Const function","Virtual function"},
+            {"Data hiding","Dynamic Typing","Dynamic binding","Dynamic loading"},
+            {"Member Variable","Member function","Class function","Classic function"},
+            {"1","5","13","0"},
+            {"Function","Class","Object","Operator function"}
+    };
+
+    public static String ans[]={
+            "Singleton class",
+            "Friend constructor",
+            "Friend function",
+            "Dynamic binding",
+            "Member function",
+            "0",
+            "Object"
+    };
+
+}
